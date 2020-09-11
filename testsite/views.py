58d8d5fc2f33f192962df8476e0fe6a5f8bc608a
HARSHIT YADAV <56081331+Harshit-1999@users.noreply.github.com>
2020-09-11 20:08:44 +0530

    Initial commit

---
+++ testsite/views.py
@@ -0,0 +1,9 @@
+# This is customised file-Harshit
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello")
+
+def index(request):
+    return HttpResponse("Home")
